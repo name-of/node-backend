197bce6e587401841159e00cd9e7a6d7234c57f3
Ayush Singh <ayushsinghiii.94@gmail.com>
2018-01-11 18:30:40 +0530

    404 on invalid calls

--- routes.js
+++ routes.js
@@ -6,7 +6,15 @@ module.exports = function(app) {
     .get(controller.homepage)
     .post(controller.page404);
 
-  app.route('/new_access_token/:account/:token')
+  app.route('/new_access_token/:userID/:account/:token')
     .get(controller.page404)
     .put(controller.newAccessTok);
+
+    app.use(function(req, res) {
+      res.status(404)
+        .send({
+          error: req.url + ' not found',
+          cause: 'api call to invalid endpoint.',
+        });
+    });
 };
